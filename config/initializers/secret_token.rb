55fd8192ca879e242cf083298b60c2fdd8513ce0
Andres <discobanco@gmail.com>
2014-06-21 12:40:33 +0100

    Initialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railsgirls::Application.config.secret_key_base = '6d296a25f70d8a4d6a93bb714e3bd452b3afe31a0c7746e2115f086f825c7ba25a368a5402b454c00fd732095a4abc7de15b35d21e8bad413109f4fb011db7f2'
